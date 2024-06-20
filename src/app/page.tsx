d824177ee52746e869b7f5391a29d7fc3e1919b1
NabilaFerdousPrapty <nabilaferdousprapty@gmail.com>
2024-06-20 21:25:09 +0600

    Navbar added

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,7 +5,7 @@ export default function Home() {
   return (
     <div>
       <Navbar/>
-      <div className="flex justify-center items-center h-screen bg-gray-100">
+      <div className="flex justify-center items-center h-screen ">
         <h1 className="text-6xl">Welcome to Lyric Lounge</h1>
 
     </div>

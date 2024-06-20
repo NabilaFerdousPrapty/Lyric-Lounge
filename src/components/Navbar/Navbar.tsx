449b2503ee056a3daa0d3f3115fa1dc6f75a9a4d
NabilaFerdousPrapty <nabilaferdousprapty@gmail.com>
2024-06-20 22:23:40 +0600

    links added

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -1,4 +1,4 @@
-'use client'
+"use client";
 import React, { useState } from "react";
 import { HoveredLink, Menu, MenuItem, ProductItem } from "../ui/navbar-menu";
 import { cn } from "@/utils/cn";
@@ -6,20 +6,28 @@ import Link from "next/link";
 
 export default function Navbar({ className }: { className?: string }) {
   const [active, setActive] = useState<string | null>(null);
- 
 
   return (
-   <div className={cn("fixed top-10 inset-x-0 max-w-2xl mx-auto z-50", className)}>
-   <Menu setActive={setActive}>
-  <Link href={'/'}>
-  <MenuItem setActive={setActive} active={active}  item="Home">
-  Home
-  </MenuItem>
-  </Link>
-  
-
-   </Menu>
-   </div>
-
-  )
+    <div
+      className={cn("fixed top-10 inset-x-0 max-w-2xl mx-auto z-50", className)}
+    >
+      <Menu setActive={setActive}>
+        <Link href={"/"}>
+          <MenuItem setActive={setActive} active={active} item="Home">
+            Home
+          </MenuItem>
+        </Link>
+        <HoveredLink href={"/about"}>
+          <MenuItem setActive={setActive} active={active} item="About">
+            About
+          </MenuItem>
+        </HoveredLink>
+        <HoveredLink href={"/ourCourses"}>
+          <MenuItem setActive={setActive} active={active} item="All Courses">
+            Our Courses
+          </MenuItem>
+        </HoveredLink>
+      </Menu>
+    </div>
+  );
 }

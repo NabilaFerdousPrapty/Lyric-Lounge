9399757b8b603049a92f244af783f47438c29d13
NabilaFerdousPrapty <nabilaferdousprapty@gmail.com>
2024-07-10 01:42:48 +0600

    hero section

--- src/components/Hero/HeroSection.tsx
+++ src/components/Hero/HeroSection.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 const HeroSection = () => {
     return (
-        <div>
+        <div className='h-auto md:h-[40rem] w-full rounded-md flex flex-col items-center justify-center relative overflow-hidden mx-auto py-10 md:py-0'>
             
         </div>
     );

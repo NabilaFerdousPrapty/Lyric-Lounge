93294341cd925b452b0a8328274e934aecb0b2d8
NabilaFerdousPrapty <nabilaferdousprapty@gmail.com>
2024-07-10 02:40:58 +0600

    hero section done

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,7 +7,7 @@ export default function Home() {
     <div>
       
       <main className="min-h-screen bg-black bg-opacity-[.96]  antialiased bg-grid-white/[0.02]">
-        <h1 className="text-2xl text-center">Welcome to Lyric Lounge</h1>
+      
        <HeroSection/>
     </main>
     </div>

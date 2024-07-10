0d8a02aacec0d96aa958bbb0011741667ec012da
NabilaFerdousPrapty <nabilaferdousprapty@gmail.com>
2024-07-11 03:42:34 +0600

    featueed courses added

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,7 @@
 import HeroSection from "@/components/Hero/HeroSection";
 import Navbar from "@/components/Navbar/Navbar";
 import Image from "next/image";
-
+import FeaturedCourses from '../components/FeaturedCourses/FeaturedCourses'
 export default function Home() {
   return (
     <div>
@@ -9,6 +9,7 @@ export default function Home() {
       <main className="min-h-screen bg-black bg-opacity-[.96]  antialiased bg-grid-white/[0.02]">
       
        <HeroSection/>
+       <FeaturedCourses/>
     </main>
     </div>
   );
---
+++ src/components/FeaturedCourses/FeaturedCourses.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const FeaturedCourses = () => {
+    return (
+        <div>
+            <h1>
+                Featured Courses
+            </h1>
+        </div>
+    );
+};
+
+export default FeaturedCourses;
